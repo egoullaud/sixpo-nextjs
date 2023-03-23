101df043b820647a58cb6deebb730ea4e8cac12b
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-23 15:25:40 -0700

    empty

--- components/Speakers.jsx
+++ components/Speakers.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Speakers() {
-  return <div></div>;
+  return (
+    <div>
+      <h1
+        className="font-bold text-4xl mx-[2rem] mb-[2rem] text-center
+          lg:text-5xl"
+      >
+        Our Speakers
+      </h1>
+    </div>
+  );
 }
 
 export default Speakers;
