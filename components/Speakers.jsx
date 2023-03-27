378cd805462a59a63764295dbd250b67713f1a5b
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-27 16:46:58 -0700

    created speakers card

--- components/Speakers.jsx
+++ components/Speakers.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 
-function Speakers() {
+function Speakers({ name, url, description, image_alt, image }) {
   return (
     <div>
-      <h1
-        className="font-bold text-4xl mx-[2rem] mb-[2rem] text-center
-          lg:text-5xl"
-      >
-        Our Speakers
-      </h1>
+      <div>
+        <div
+          className="flex flex-col justify-center items-center m-4 
+        lg:justify-center"
+        >
+          <img
+            className="rounded w-[90%]
+            md:w-[50%]
+            lg:w-[100%] lg:max-h-64 lg:object-cover lg:object-top"
+            src={image}
+            alt={image_alt}
+          />
+          <h2 className="font-bold text-xl my-2">{name}</h2>
+          <a href={url} target="_blank">
+            <h2 className="text-lg text-blue-600">Visit their website</h2>
+          </a>
+          <p
+            className="w-[90%] my-2
+           md:text-lg md:w-[75%]
+           lg:w-[90%] lg:text-center"
+          >
+            {description}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
