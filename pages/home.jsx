1fe8c7abf4d3d32d59a279cad50c7af1767e7fed
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-28 14:08:56 -0700

    added query to services

--- pages/home.jsx
+++ pages/home.jsx
@@ -7,30 +7,13 @@ import HomeCards from "@/components/HomeCards";
 import SponsorCard from "@/components/SponsorCard";
 import Donations from "@/components/Donations";
 import Form from "@/components/Form";
-import { GraphQLClient, gql } from "graphql-request";
+import { GraphQLClient } from "graphql-request";
+import { SPONSORS_QUERY } from "@/services";
 
-const hygraph = new GraphQLClient(`${process.env.HYGRAPH_URL}`);
-const SPONSORS_QUERY = gql`
-  {
-    sponsors {
-      resources {
-        id
-        slug
-        title
-        url
-        image {
-          url
-          altText
-        }
-        content {
-          html
-        }
-      }
-    }
-  }
-`;
 export async function getStaticProps() {
-  const { sponsors } = await hygraph.request(SPONSORS_QUERY);
+  const { sponsors } = await new GraphQLClient(
+    `${process.env.HYGRAPH_URL}`
+  ).request(SPONSORS_QUERY);
   return {
     props: {
       sponsors,
