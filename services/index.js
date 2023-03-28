e9841c6f1be24274f89b3e3d1994473e50737536
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-28 14:09:31 -0700

    created queries

---
+++ services/index.js
@@ -0,0 +1,118 @@
+import { gql } from "graphql-request";
+
+export const BLOG_QUERY = gql`
+  {
+    posts {
+      id
+      slug
+      title
+      image {
+        url
+        altText
+      }
+      category {
+        id
+        slug
+        title
+      }
+      authors {
+        name
+        id
+        content {
+          html
+        }
+        image {
+          id
+          url
+          altText
+        }
+      }
+      datePublished
+      preview
+    }
+  }
+`;
+
+export const POST_BY_SLUG_QUERY = gql`
+  query Post($slug: String!) {
+    post(where: { slug: $slug }) {
+      id
+      slug
+      title
+      content {
+        html
+      }
+      image {
+        url
+        altText
+      }
+      category {
+        id
+        slug
+        title
+      }
+      authors {
+        name
+        id
+        content {
+          html
+        }
+        image {
+          id
+          url
+          altText
+        }
+      }
+      datePublished
+    }
+  }
+`;
+
+export const POST_SLUGLIST = gql`
+  {
+    posts {
+      slug
+    }
+  }
+`;
+
+export const RESOURCE_QUERY = gql`
+  {
+    resources {
+      id
+      slug
+      title
+      url
+      category {
+        title
+      }
+      content {
+        html
+      }
+      image {
+        url
+        altText
+      }
+    }
+  }
+`;
+
+export const SPONSORS_QUERY = gql`
+  {
+    sponsors {
+      resources {
+        id
+        slug
+        title
+        url
+        image {
+          url
+          altText
+        }
+        content {
+          html
+        }
+      }
+    }
+  }
+`;
