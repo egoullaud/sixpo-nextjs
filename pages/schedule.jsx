d9176037051a34b87de8a4c136587e25c0a768d1
elizabeth.goullaud <egoullaud@gmail.com>
2023-03-30 16:14:24 -0700

    made events available

--- pages/schedule.jsx
+++ pages/schedule.jsx
@@ -17,20 +17,20 @@ export async function getStaticProps() {
 
 function schedule({ schedules }) {
   return (
-    <div className="flex flex-col  bg-black bg-opacity-70 pb-[10rem] h-[75vh] ">
+    <div className="flex flex-col  bg-black bg-opacity-70 pb-[10rem] ">
       <h1
         className="font-bold w-[100%] text-white text-center mt-[1rem] text-5xl
          md:text-5xl md:mt-[5rem] md:mb-[1rem]
          lg:text-7xl lg:mt-[7rem] lg:mb-[3rem]"
       >
-        Event Schedule <br></br> Coming soon!
+        Event Schedule
       </h1>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center mt-4">
         <Link href="/events">
           <button
             className="
                       text-white bg-[#ff5b5b] rounded-lg font-bold shadow-lg
-                      py-2 px-4 mb-4 
+                      py-2 px-4 mb-4 mr-2
                       md:py-2 md:px-4 md:mx-5
                       lg:px-8 lg:py-3 lg:text-lg 
                       hover:ease-in-out hover:duration-500 hover:bg-[#ff7070]"
@@ -42,7 +42,7 @@ function schedule({ schedules }) {
           <button
             className="
                       text-white bg-[#ff5b5b] rounded-lg font-bold shadow-lg
-                      py-2 px-4 mb-4 
+                      py-2 px-4 mb-4 ml-2
                       md:py-2 md:px-4 md:mx-5
                       lg:px-8 lg:py-3 lg:text-lg 
                       hover:ease-in-out hover:duration-500 hover:bg-[#ff7070]"
@@ -54,19 +54,21 @@ function schedule({ schedules }) {
 
       {/*   flex flex-col justify-center items-center */}
       <div
-        className=" hidden 
-    
+        className="grid mx-4
+        md:grid-cols-2 md:gap-4
+        lg:grid-cols-3
+        xl:grid-cols-4
       "
       >
         {schedules.map((schedule) => (
           <EventCard
             key={schedule.id}
             title={schedule.title}
-            eventDate={schedule.eventDate}
+            startDate={schedule.startDate}
+            endTime={schedule.endTime}
             active={schedule.active}
             briefDescription={schedule.briefDescription}
             zoomLink={schedule.zoomLink}
-            description={schedule.description.html}
           />
         ))}
       </div>
