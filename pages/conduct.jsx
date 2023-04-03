508ce4c1c19d6def752f5580abc612d178ed8e0a
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-03 13:18:32 -0700

    content added

--- pages/conduct.jsx
+++ pages/conduct.jsx
@@ -2,13 +2,197 @@ import React from "react";
 
 function conduct() {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center my-[4rem]">
       <h1
         className="font-bold m-4 text-center text-2xl my-[2rem]
-      lg:text-4xl"
+        md:text-3xl
+    lg:text-4xl xl:text-5xl"
       >
-        Code of Conduct
+        Sixpo's Code of Conduct
       </h1>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[4rem] mb-4">
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          SIXPO, posAbilities, Burnaby Association for Community Inclusion,
+          Options for Sexual Health and other SIXPO affiliates (“SIXPO and
+          Affiliates”) require all participants, attendees, speakers, guests,
+          professionals, press, staff, volunteers and security (“Attendees of
+          SIXPO”) to abide by common-sense rules for public behavior and
+          personal interaction. SIXPO is a celebration of the sexuality of
+          people with disabilities, and any disrespectful or non-consensual
+          behavior will not be tolerated in any form. Whenever possible, given
+          staffing capacity, SIXPO team members will attempt to address and
+          resolve any conflict resulting from disrespectful or non-consensual
+          behavior. SIXPO and Affiliates reserve the right to revoke admission
+          or participation of Attendees of SIXPO not in compliance with this
+          Code of Conduct.
+          <br /> <br />
+          By attending SIXPO, each Attendee of SIXPO agrees to this Code of
+          Conduct.
+          <br /> <br />
+          Anyone who feels they are in a situation where their safety is
+          compromised or who become aware of anyone violating any part of this
+          Code of Conduct should locate the nearest SIXPO team member so the
+          matter can be handled. SIXPO team members will take prompt action in
+          any form they deem appropriate, including immediate expulsion from the
+          event. This Code of Conduct applies to all Attendees of SIXPO. If at
+          any time you have been made to feel uncomfortable, or if you witness
+          another person being harassed, you should contact a SIXPO team member.
+          If necessary, we will contact local law enforcement, provide escort,
+          offer a safe place or otherwise assist those experiencing harassment.
+          We appreciate everyone’s cooperation and understanding in keeping our
+          event protected and making it an event that gives individuals and
+          groups the confidence to express themselves safely and without
+          discrimination.
+        </p>
+      </div>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[2rem] mb-4">
+        <h2 className="font-bold text-xl xl:text-2xl my-2">Zero Tolerance</h2>
+        <ol className="w-[100%] md:px-[2rem] xl:text-xl list-customAlpha mb-2">
+          Sixpo has zero tolerance for:
+          <li className="ml-[4rem] my-1">
+            physical and sexual assault or battery;
+          </li>
+          <li className="ml-[4rem] mb-1">
+            unwelcome physical contact of any kind; and
+          </li>
+          <li className="ml-[4rem]">harassment of any kind.</li>
+        </ol>
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          Participants asked to stop any of the above behaviour, including
+          harassing behavior, are expected to comply immediately.
+          <br /> <br />
+          Harassment includes, but is not limited to: offensive verbal comments
+          (related to gender, gender identity and expression, sexual
+          orientation, disability, physical appearance, body size, race,
+          religion), deliberate intimidation, stalking, harassing photography or
+          recording.
+        </p>
+      </div>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[4rem] mb-4">
+        <h2 className="font-bold text-xl xl:text-2xl my-2">Photography</h2>
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          Cameras and devices with cameras are allowed, but if an individual
+          declines to pose, you may not photograph or record them. Photos or
+          recordings in non-public spaces such as restrooms are not allowed.
+        </p>
+      </div>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[4rem] mb-4">
+        <h2 className="font-bold text-xl xl:text-2xl my-2">
+          Harassment in Public Restrooms
+        </h2>
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          Attendees of SIXPO are welcome to use the restrooms that match their
+          gender presentation or identity.
+        </p>
+      </div>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[4rem] mb-4">
+        <h2 className="font-bold text-xl xl:text-2xl my-2">
+          Public Nudity and Sexual Activity
+        </h2>
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          Public nudity is not permitted.
+          <br /> <br />
+          Sexual activity is not permitted. Sexual activity includes, but is not
+          limited to, simulation of such acts (faked masturbation, lap dances,
+          the use of adult toys entering any orifice, etc.). SIXPO may include
+          educational demonstrations of uses of sex toys and different sexual
+          acts for the purposes of the workshops and seminars. If you are
+          participating in a workshop that involves demonstrations of sexual
+          activity, non-consensual behaviour towards others will not be
+          tolerated.
+        </p>
+      </div>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[4rem] mb-4">
+        <h2 className=" font-bold text-xl xl:text-2xl my-2">COVID-19</h2>
+        <ol className="w-[100%] md:px-[2rem] xl:text-xl list-customAlpha mb-2">
+          Attendees of SIXPO must comply with all set procedures to reduce the
+          spread of COVID-19 while attending SIXPO, and must only attend if
+          they:
+          <li className="ml-[4rem] my-2">
+            are not experiencing any symptoms of illness such as cough,
+            shortness of breath or difficulty breathing, fever, chills, repeated
+            shaking with chills, muscle pain, headache, sore throat, or new loss
+            of taste or smell;
+          </li>
+          <li className="ml-[4rem] mb-2">
+            do not believe they have been exposed to someone with a suspected
+            and/or confirmed case of the COVID-19;
+          </li>
+          <li className="ml-[4rem] mb-2">
+            have not been diagnosed with COVID-19, or have been cleared as
+            non-contagious by the province or local public health authorities;
+            and
+          </li>
+          <li className="ml-[4rem] mb-2">
+            are following all BC Public Health recommended guidelines as much as
+            possible and limiting potential exposure to COVID-19.
+          </li>
+        </ol>
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          Due to the changing nature of the pandemic, and in order to be
+          compliant with relevant government regulations, it may be necessary
+          for SIXPO to update its COVID-19 policy. We will inform registered
+          participants about substantial changes should this occur.
+        </p>
+      </div>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[4rem] mb-4">
+        <h2 className="font-bold text-xl xl:text-2xl my-2">
+          Warranties and Liability
+        </h2>
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          SIXPO and Affiliates give no warranties in respect of any aspect of
+          SIXPO or any materials related thereto or offered at SIXPO and
+          disclaim all implied warranties, including but not limited to
+          warranties of fitness for a particular purpose, accuracy, timeliness,
+          and merchantability. SIXPO and Affiliates shall not be responsible or
+          liable for reliance by anyone on any aspect of SIXPO and/or any
+          information provided at SIXPO. Views expressed by speakers, sponsors,
+          or exhibitors are their own. In addition, SIXPO and Affiliates shall
+          not be responsible or liable for any direct or indirect loss or damage
+          of any sort incurred as the result of the services or information
+          provided in connection with the event.
+          <br /> <br />
+          Attendees of SIXPO agree that SIXPO involves many very positive
+          aspects and also can involve various risks, dangers and hazards, which
+          can result in SERIOUS INJURIES, including MENTAL OR EMOTIONAL TRAUMA.
+          The parties are aware of the risks, dangers and hazards associated
+          with SIXPO and freely accept and fully assume all such risks, dangers
+          and hazards and the possibility of personal injury or loss resulting
+          from participating in SIXPO.
+        </p>
+        <ol className="w-[100%] md:px-[2rem] xl:text-xl list-customAlpha my-2">
+          By attending SIXPO, each Attendee of SIXPO:
+          <li className="ml-[4rem] my-2">
+            waives any and all claims that they have or may in the future have
+            against SIXPO and its affiliates and releases SIXPO and its
+            affiliates from any and all liability for any loss, damage, expense
+            or injury including death that they may suffer or that their next of
+            kin may suffer as a result of participation in SIXPO, due to any
+            cause whatsoever, including negligence, breach of contract, or
+            breach of any statutory or other duty of care, including any duty of
+            care owed under the Occupiers Liability Act, RSBC 1996, c 337, on
+            the part of SIXPO and Affiliates;
+          </li>
+          <li className="ml-[4rem] mb-2">
+            understands that negligence includes failure on the part of SIXPO
+            and Affiliates to take reasonable steps to safeguard or protect any
+            participant from or warn any participant of the risks, dangers and
+            hazards of participating in SIXPO;
+          </li>
+          <li className="ml-[4rem] mb-2">
+            indemnifies and holds harmless SIXPO and Affiliates from any and all
+            claims, actions, causes of action, injury, losses or damages, which
+            may be made by any participant related to the SIXPO, or anyone on
+            behalf of any participant related to the Attendee of SIXPO; and
+          </li>
+          <li className="ml-[4rem] mb-2">
+            indemnifies and holds harmless SIXPO and Affiliates from any and all
+            liability for any damage to property or personal injury to any third
+            party, resulting from the participation in SIXPO of any Attendee of
+            SIXPO.
+          </li>
+        </ol>
+      </div>
     </div>
   );
 }
