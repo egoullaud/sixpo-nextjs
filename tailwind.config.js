08b4dbe3fd9b622693e6cfa6c6938bd6e9a50847
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-03 13:18:44 -0700

    alphabet list style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      listStyleType: {
+        customAlpha: "lower-alpha",
+      },
+    },
   },
   plugins: [],
 };
