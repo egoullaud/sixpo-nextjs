0f16ef2622ef808cc5906c9be592b2ebf7fe307d
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-03 12:51:41 -0700

    added content /formatted

--- pages/agreement.jsx
+++ pages/agreement.jsx
@@ -2,13 +2,74 @@ import React from "react";
 
 function agreement() {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center my-[4rem]">
       <h1
         className="font-bold m-4 text-center text-2xl my-[2rem]
-      lg:text-4xl"
+      lg:text-4xl xl:text-5xl"
       >
-        Community Agreement
+        Community Agreements
       </h1>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[4rem] mb-4">
+        <h2 className="font-bold text-xl xl:text-2xl my-2">What is SIXPO?</h2>
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          SIXPO (Sexuality, Inclusion and Exploration) is a festival organized
+          by folks with disabilities for their community, where we learn
+          together about topics related to sexual wellness. There will be panel
+          discussions, guest speakers, and workshops, where you can have
+          conversations with peers and experts!{" "}
+        </p>
+      </div>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[4rem] mb-4">
+        <h2 className="font-bold text-xl xl:text-2xl my-2">
+          Who is SIXPO for?
+        </h2>
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          SIXPO is for folks who are curious or want to learn more about
+          sexuality. Whether you have never gone on a date, or you feel like you
+          are a sexual expert, SIXpo is for you!
+        </p>
+      </div>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[4rem] mb-4">
+        <h2 className="font-bold text-xl xl:text-2xl my-2">Why SIXPO?</h2>
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          There aren’t many events that celebrate the sexuality of people with
+          disabilities. SIXPO is a festival where you can come to learn, ask
+          questions, and share your experiences about sexuality. Whether these
+          conversations are new to you, or you are confident in talking about
+          sexuality, we will have events for you!
+        </p>
+      </div>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[4rem] mb-4">
+        <h2 className="font-bold text-xl xl:text-2xl my-2">Questions?</h2>
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          Need help understanding something you heard in a presentation or
+          workshop? Find a sex educator or a volunteer! They will be happy to
+          answer your questions.
+        </p>
+      </div>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[4rem] mb-4">
+        <h2 className="font-bold text-xl xl:text-2xl my-2">Self-Advocacy</h2>
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          You are your own self-advocate! At SIXPO, you get to choose what you
+          want to learn, and you’re always allowed to change your mind! It’s
+          okay if you don’t like a workshop, a topic,or a conversation. You can
+          leave anytime, and we’ll be happy you took care of yourself! There is
+          a Wellness space you can visit if you feel overwhelmed, or want a
+          break.
+        </p>
+      </div>
+      <div className="w-[90%] flex flex-col justify-center items-center md:mx-[4rem] mb-4">
+        <h2 className=" font-bold text-xl xl:text-2xl my-2">Public Event</h2>
+        <p className="md:mx-[2rem] xl:mx-[5rem] xl:text-xl">
+          This is a public event. You are encouraged to invite people and make
+          new friends! If you feel uncomfortable and need to talk to someone,
+          please visit the Wellness space or find a volunteer.
+          <br /> <br />
+          We don’t tolerate verbal or physical violence, sexual harassment, or
+          discrimination at SIXPO. If you do any of these things, we will ask
+          you to leave.
+        </p>
+      </div>
     </div>
   );
 }
