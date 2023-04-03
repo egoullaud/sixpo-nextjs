b3a1960361adfaa4132a28f3b29ca93b7081cdc7
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-03 12:37:21 -0700

    created conduct page

---
+++ pages/conduct.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function conduct() {
+  return (
+    <div>
+      <h1
+        className="font-bold m-4 text-center text-2xl my-[2rem]
+      lg:text-4xl"
+      >
+        Code of Conduct
+      </h1>
+    </div>
+  );
+}
+
+export default conduct;
