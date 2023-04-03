ede39168736cd9ce907341deeb952e82602b4665
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-03 12:37:15 -0700

    created agreeent page

---
+++ pages/agreement.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function agreement() {
+  return (
+    <div>
+      <h1
+        className="font-bold m-4 text-center text-2xl my-[2rem]
+      lg:text-4xl"
+      >
+        Community Agreement
+      </h1>
+    </div>
+  );
+}
+
+export default agreement;
