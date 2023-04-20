1d987ca52d6ee411dc7faf64efb2df7aa7e20c65
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-20 15:09:32 -0700

    sorry no events added

--- components/CurrentEvents.jsx
+++ components/CurrentEvents.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import moment from "moment";
 import Link from "next/link";
 import TodayEventCard from "./TodayEventCard";
+import inperson from "@/pages/schedule/inperson";
+import EventCard from "./EventCard";
 
 function CurrentEvents({ schedules, inPersonEvents }) {
   const today = moment().format("YYYY-MM-DD");
@@ -20,9 +22,44 @@ function CurrentEvents({ schedules, inPersonEvents }) {
     );
   });
 
-  let content;
+  let inPersonContent;
+  // dsplay inperson events today
+  if (inPersonToday.length > 0) {
+    inPersonContent = (
+      <div className="flex flex-col items-center justify-center mx-[1rem]">
+        <Link href="/schedule/inperson">
+          {" "}
+          <button
+            className=" text-white bg-[#ff5b5b] rounded-lg font-bold shadow-lg
+                    py-2 px-4
+                    md:py-2 md:px-4 
+                    lg:px-8 lg:py-3 lg:text-lg 
+                    hover:transition-all hover:duration-500 hover:bg-[#ff7070]  ease-out duration-500"
+          >
+            See All In Person Events
+          </button>
+        </Link>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {inPersonToday.map((inPersonEvent) => (
+            <TodayEventCard
+              key={inPersonEvent.id}
+              title={inPersonEvent.title}
+              startTime={inPersonEvent.startTime}
+              endTime={inPersonEvent.endTime}
+              speaker={inPersonEvent.speaker}
+              location="Heritage Hall"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    inPersonContent = <p className="my-2">Sorry, no in person events today</p>;
+  }
+
+  let virtualContent;
   if (todaysEvents.length > 0) {
-    content = (
+    virtualContent = (
       <div className="flex flex-col items-center justify-center mx-[1rem]">
         <Link href="/schedule">
           {" "}
@@ -50,37 +87,8 @@ function CurrentEvents({ schedules, inPersonEvents }) {
         </div>
       </div>
     );
-  } else if (inPersonToday.length > 0) {
-    content = (
-      <div className="flex flex-col items-center justify-center mx-[1rem]">
-        <Link href="/schedule/inperson">
-          {" "}
-          <button
-            className=" text-white bg-[#ff5b5b] rounded-lg font-bold shadow-lg
-                      py-2 px-4
-                      md:py-2 md:px-4 
-                      lg:px-8 lg:py-3 lg:text-lg 
-                      hover:transition-all hover:duration-500 hover:bg-[#ff7070]  ease-out duration-500"
-          >
-            See Event Schedule
-          </button>
-        </Link>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          {inPersonToday.map((inPersonEvent) => (
-            <TodayEventCard
-              key={inPersonEvent.id}
-              title={inPersonEvent.title}
-              startTime={inPersonEvent.startTime}
-              endTime={inPersonEvent.endTime}
-              speaker={inPersonEvent.speaker}
-              location="Heritage Hall"
-            />
-          ))}
-        </div>
-      </div>
-    );
   } else {
-    content = <p>Sorry, no events today</p>;
+    virtualContent = <p className="my-2">Sorry, no online events today</p>;
   }
   return (
     <div className="flex flex-col justify-center items-center bg-gradient-to-b from-[#bcf0ff] pt-[1rem] pb-[4rem]">
@@ -92,9 +100,8 @@ function CurrentEvents({ schedules, inPersonEvents }) {
       >
         Today's Events
       </h1>
-      <div className="flex flex-col md:flex-row md:flex-wrap items-center justify-around mx-[2rem]">
-        {content}
-      </div>
+      <div>{virtualContent}</div>
+      <div>{inPersonContent}</div>
     </div>
   );
 }
