06c08846a152983c65a78ccf3d51ef289088fe4b
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-20 13:41:03 -0700

    added new card

--- components/CurrentEvents.jsx
+++ components/CurrentEvents.jsx
@@ -1,12 +1,54 @@
 import React from "react";
 import moment from "moment";
-import EventCard from "./EventCard";
 
-function CurrentEvents({ schedules }) {
+import TodayEventCard from "./TodayEventCard";
+
+function CurrentEvents({ schedules, inPersonEvents }) {
   const today = moment().format("YYYY-MM-DD");
   const todaysEvents = schedules.filter((schedule) => {
     return moment(schedule.startDate).format("YYYY-MM-DD") === today;
   });
+  const inPersonToday = inPersonEvents.filter((inPersonEvent) => {
+    return (
+      moment(inPersonEvent.startDate).format("YYYY-MM-DD") === today &&
+      !inPersonEvent.isBreak
+    );
+  });
+
+  let content;
+  if (todaysEvents.length > 0) {
+    content = (
+      <div className="flex flex-col md:flex-row md:flex-wrap items-center justify-around mx-[2rem]">
+        {todaysEvents.map((schedule) => (
+          <TodayEventCard
+            key={schedule.id}
+            title={schedule.title}
+            startTime={schedule.startTime}
+            endTime={schedule.endTime}
+            speaker={schedule.speaker}
+            location="Virtual"
+          />
+        ))}
+      </div>
+    );
+  } else if (inPersonToday.length > 0) {
+    content = (
+      <div className="flex flex-col md:flex-row md:flex-wrap items-center justify-around mx-[2rem]">
+        {inPersonToday.map((inPersonEvent) => (
+          <TodayEventCard
+            key={inPersonEvent.id}
+            title={inPersonEvent.title}
+            startTime={inPersonEvent.startTime}
+            endTime={inPersonEvent.endTime}
+            speaker={inPersonEvent.speaker}
+            location="In Person"
+          />
+        ))}
+      </div>
+    );
+  } else {
+    content = <p>Sorry, no events today</p>;
+  }
   return (
     <div className="flex flex-col justify-center items-center bg-gradient-to-b from-[#bcf0ff] pt-[1rem] pb-[4rem]">
       <h1
@@ -18,9 +60,7 @@ function CurrentEvents({ schedules }) {
         Today's Events
       </h1>
       <div className="flex flex-col md:flex-row md:flex-wrap items-center justify-around mx-[2rem]">
-        {todaysEvents.map((schedule) => (
-          <EventCard key={schedule.id} {...schedule} />
-        ))}
+        {content}
       </div>
     </div>
   );
