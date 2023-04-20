efb93cf0649bbd48e30a2a654f6b872c00ffb0b8
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-20 14:25:16 -0700

    added styling

--- components/TodayEventCard.jsx
+++ components/TodayEventCard.jsx
@@ -5,17 +5,16 @@ function TodayEventCard({ key, title, startTime, endTime, speaker, location }) {
   return (
     <div
       key={key}
-      className=" bg-white rounded shadow-md flex flex-col items-center justify-center px-4 pb-4"
+      className=" bg-white rounded shadow-md flex flex-col items-center justify-center p-4 m-4"
     >
-      <h1>{title}</h1>
+      <h1 className="text-2xl font-bold text-center">{title}</h1>
       {startTime && (
-        <h2 className="font-bold text-3xl text-white text-center my-[4rem]">
-          {moment(startTime).format("dddd, MMMM D")}-{" "}
-          {moment(endTime).format("h:mm A")}
+        <h2 className="text-xl text-center">
+          {moment(startTime).format("h:mm")}- {moment(endTime).format("h:mm A")}
         </h2>
       )}
-      {speaker && <h3>{speaker}</h3>}
-      {location && <p>{location}</p>}
+      {speaker && <h3 className="text-center my-1">Speaker: {speaker}</h3>}
+      {location && <p>Location: {location}</p>}
     </div>
   );
 }
